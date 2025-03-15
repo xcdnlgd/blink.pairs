3438406fc1cfb9a3ada5c46b1c408faf49e9e29c
Liam Dyer <liamcdyer@gmail.com>
2025-03-15 12:45:49 -0400

    feat: escaped and space mappings

--- lua/blink/pairs/mappings.lua
+++ lua/blink/pairs/mappings.lua
@@ -10,6 +10,7 @@
 --- @field closing string
 --- @field enter? boolean
 --- @field backspace? boolean
+--- @field space? boolean
 ---
 --- @alias blink.pairs.PairsStrict table<string, blink.pairs.PairStrict>
 
@@ -41,6 +42,7 @@ function mappings.register(pairs)
 
   map('<BS>', mappings.backspace(pairs))
   map('<CR>', mappings.enter(pairs))
+  map('<Space>', mappings.space(pairs))
 end
 
 function mappings.shift_backward_keycode(amount) return string.rep('<C-g>u<Left>', amount) end
@@ -48,7 +50,12 @@ function mappings.shift_backward_keycode(amount) return string.rep('<C-g>u<Left>
 function mappings.shift_forward_keycode(amount) return string.rep('<C-g>u<Right>', amount) end
 
 function mappings.open_pair(opening, closing)
-  return function() return opening .. closing .. mappings.shift_backward_keycode(1) end
+  return function()
+    -- "\|" -> "\(|"
+    if mappings.is_escaped() then return opening end
+    -- "|" -> "(|)"
+    return opening .. closing .. mappings.shift_backward_keycode(1)
+  end
 end
 
 function mappings.close_pair(closing)
@@ -56,7 +63,13 @@ function mappings.close_pair(closing)
     local cursor = vim.api.nvim_win_get_cursor(0)
     local line = vim.api.nvim_get_current_line()
     local next_char = line:sub(cursor[2] + 1, cursor[2] + 1)
+    local next_next_char = line:sub(cursor[2] + 2, cursor[2] + 2)
+
+    -- "|)" -> ")|"
     if next_char == closing then return mappings.shift_forward_keycode(1) end
+    -- "| )" -> " )|"
+    if next_char == ' ' and next_next_char == closing then return mappings.shift_forward_keycode(2) end
+
     return closing
   end
 end
@@ -66,7 +79,11 @@ function mappings.open_or_close_pair(pair)
     local cursor = vim.api.nvim_win_get_cursor(0)
     local line = vim.api.nvim_get_current_line()
     local next_char = line:sub(cursor[2] + 1, cursor[2] + 1)
+
+    -- "|'" -> "'|"
     if next_char == pair then return mappings.close_pair(pair)() end
+
+    -- "|" -> "'|'"
     return mappings.open_pair(pair, pair)()
   end
 end
@@ -75,6 +92,7 @@ end
 function mappings.backspace(pairs)
   return function()
     local pair = mappings.get_current_pair(pairs)
+    -- "(|)" -> "|"
     if pair ~= nil and pair.backspace ~= false then return mappings.shift_forward_keycode(1) .. '<BS><BS>' end
 
     return '<BS>'
@@ -85,9 +103,24 @@ end
 function mappings.enter(pairs)
   return function()
     local pair = mappings.get_current_pair(pairs)
+    -- "(|)" ->
+    -- (
+    --   |
+    -- )
     if pair ~= nil and pair.enter ~= false then return '<CR><C-o>O' end
 
     return '<CR>'
+  end
+end
+
+--- @param pairs blink.pairs.PairsStrict
+function mappings.space(pairs)
+  return function()
+    local pair = mappings.get_current_pair(pairs)
+    -- "(|)" -> "( | )"
+    if pair ~= nil and pair.space ~= false then return '<Space><Space><C-g>u<Left>' end
+
+    return '<Space>'
   end
 end
 
@@ -102,6 +135,13 @@ function mappings.get_current_pair(pairs)
 
   local def = pairs[prev_char]
   if def ~= nil and next_char == def.closing then return def end
+end
+
+function mappings.is_escaped()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local line = vim.api.nvim_get_current_line()
+
+  return line:sub(cursor[2], cursor[2]) == '\\'
 end
 
 return mappings
