b08c2a27988c03c74778423c892804e659ac3568
Liam Dyer <liamcdyer@gmail.com>
2025-03-25 11:31:51 -0400

    feat: add benchmarks

---
+++ benches/lib.rs
@@ -0,0 +1,19 @@
+use blink_pairs::parser::{parse_filetype, ParseState};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("parse - rust", |b| {
+        let text = include_str!("./languages/rust.rs");
+        let lines = text.lines().collect::<Vec<_>>();
+        b.iter(|| parse_filetype("rust", black_box(&lines), ParseState::Normal))
+    });
+
+    c.bench_function("parse - c", |b| {
+        let text = include_str!("./languages/c.c");
+        let lines = text.lines().collect::<Vec<_>>();
+        b.iter(|| parse_filetype("c", black_box(&lines), ParseState::Normal))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
