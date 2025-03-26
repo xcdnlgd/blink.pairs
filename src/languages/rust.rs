00e0a2d939513faccbada2b062fb0cb277af7da5
Stefan Boca <stefan.r.boca@gmail.com>
2025-03-26 13:03:06 -0700

    fix(rust): remove raw `r""` strings
    The string regex was interfering with the parsing of the closing `"`, so
    it was never emitted

--- src/languages/rust.rs
+++ src/languages/rust.rs
@@ -9,5 +9,5 @@ define_token_enum!(RustToken, {
     line_comment: ["//"],
     block_comment: ["/*" => "*/"],
     string_regex: ["(?&dstring)", "(?&schar)"],
-    block_string: ["r\"" => "\"", "r#\"" => "\"#", "r##\"" => "##\"", "r###\"" => "###\""]
+    block_string: ["r#\"" => "\"#", "r##\"" => "##\"", "r###\"" => "###\""]
 });
